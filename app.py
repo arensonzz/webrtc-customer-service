8f3a24fbf9c5ce5cb075830cc1b6d770983f95ff
arensonz <echelon_erkan@hotmail.com>
2022-06-05 19:45:27 +0300

    Add different options to start app (HTTP, HTTPS)

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask_socketio import SocketIO
 from app import create_app, socketio
+import os
 
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Uncomment following line to import SSL certificates to the development server.
-    # You should have self-signed certificates ready in the `certificate` directory.
-    socketio.run(app, ssl_context='adhoc')
-    #  socketio.run(app)
+
+    # Make sure env variables are set
+    if not os.environ.get("HTTPS_MODE"):
+        raise RuntimeError("HTTPS_MODE not set.\n\tOptions: \"self-signed\", \"adhoc\", \"none\".")
+
+    https_mode = os.environ.get("HTTPS_MODE").lower()
+
+    if https_mode == "self-signed":
+        # You should have self-signed certificates ready in the `certificate` directory.
+        socketio.run(app, ssl_context=('certificate/fullchain.pem', 'certificate/cert-key.pem'))
+    elif https_mode == "adhoc":
+        # Mock HTTPS server
+        # You need to install `pyopenssl` package to use this option
+        socketio.run(app, ssl_context='adhoc')
+    elif https_mode == "none":
+        # HTTP server instead of HTTPS
+        socketio.run(app)
+    else:
+        raise RuntimeError("Unknown HTTPS_MODE option.")
