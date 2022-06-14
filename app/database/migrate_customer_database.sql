1dfffc9ebd979d4128bc7896ce69436affab5a0d
arensonz <echelon_erkan@hotmail.com>
2022-06-15 00:58:47 +0300

    Non-breaking changes

--- app/database/migrate_customer_database.sql
+++ app/database/migrate_customer_database.sql
@@ -2,9 +2,9 @@
 CREATE SCHEMA main;
 
 -- firm's main database schema
-CREATE TABLE IF NOT EXISTS main.customers (
+CREATE TABLE IF NOT EXISTS main.customer (
     m_customer_id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    m_phone_number text NOT NULL UNIQUE,
+    m_phone_number text NOT NULL UNIQUE, -- phone numbers are stored in E.164 standard
     m_full_name text NOT NULL,
     m_short_name text NOT NULL, -- how to address the customer
     m_email_address citext UNIQUE -- email address is (in practice) case insensitive
@@ -13,13 +13,15 @@ CREATE TABLE IF NOT EXISTS main.customers (
 -- webrtc-customer-service (wcs)
 CREATE SCHEMA wcs;
 
-CREATE OR REPLACE VIEW wcs.customers AS
+DROP TABLE IF EXISTS wcs.customer;
+
+CREATE OR REPLACE VIEW wcs.customer AS
 SELECT
-    m_customer_id AS customer_id,
+    m_customer_id AS cust_id,
     m_phone_number AS phone_number,
     m_full_name AS full_name,
     m_short_name AS short_name,
     m_email_address AS email_address
 FROM
-    main.customers;
+    main.customer;
 
